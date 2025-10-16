ea4bd0b418af49fe22cb9b1fe5d0edc9bed21e02
Bruno da Silva <95040767+Paraoxx@users.noreply.github.com>
2025-10-16 15:01:59 -0400

    refatoracao
    refatoracao codigo.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
-// Importa o seletor.js
+// Import do seletor
 import { expensesTbody } from './modules/1_selectors.js';
 
-// resto dos imports
+// Importde para iniciar
 import { initAddModal } from './modules/3_modalAdd.js';
 import { initTable } from './modules/4_table.js';
 import { initGastosModal } from './modules/5_modalGastos.js';
 import { initGanhosModal } from './modules/6_modalGanhos.js';
 
-//import de funçaõ de para calcular em linhas
 import { updateRecordsCount, recalculateAndDisplayTotals } from './modules/2_utils.js';
+
+// O "ouvinte" principal que inicia a aplicação
 document.addEventListener('DOMContentLoaded', () => {
     
     if (expensesTbody) {
         
+        //eventual ligação dos events no back
         initAddModal();
         initTable();
         initGastosModal();
         initGanhosModal();
+        
+        // count para o começo da pagina
         updateRecordsCount(); 
         recalculateAndDisplayTotals();
     }
