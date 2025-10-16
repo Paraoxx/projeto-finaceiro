951d79a70e135f055740875b65ec91a0304e7ae7
Bruno da Silva <95040767+Paraoxx@users.noreply.github.com>
2025-10-16 14:51:57 -0400

    refatoracao
    refatoracao codigo.

---
+++ js/modules/1_selectors.js
@@ -0,0 +1,26 @@
+export const expensesTbody = document.getElementById('expenses-tbody');
+export const recordsCountEl = document.getElementById('records-count');
+export const totalEntradaEl = document.getElementById('total-entrada');
+export const totalSaidaEl = document.getElementById('total-saida');
+
+// modal adicionar gasto
+export const openModalBtn = document.getElementById('openModalBtn');
+export const closeModalBtn = document.getElementById('closeModalBtn');
+export const modalOverlay = document.getElementById('addExpenseModal');
+export const addExpenseForm = document.getElementById('addExpenseForm');
+
+// modal de analise de gastos
+export const analysisCard = document.getElementById('analysis-card');
+export const analysisModal = document.getElementById('analysisModal');
+export const closeAnalysisModalBtn = document.getElementById('closeAnalysisModalBtn');
+export const analysisList = document.getElementById('analysis-list');
+export const sortDescBtn = document.getElementById('sort-desc-btn');
+export const sortAscBtn = document.getElementById('sort-asc-btn');
+
+// modal de análise de ganhos
+export const analysisGanhosCard = document.getElementById('analysis-ganhos-card');
+export const analysisGanhosModal = document.getElementById('analysisGanhosModal');
+export const closeAnalysisGanhosModalBtn = document.getElementById('closeAnalysisGanhosModalBtn');
+export const analysisGanhosList = document.getElementById('analysis-ganhos-list');
+export const sortGanhosDescBtn = document.getElementById('sort-ganhos-desc-btn');
+export const sortGanhosAscBtn = document.getElementById('sort-ganhos-asc-btn');
